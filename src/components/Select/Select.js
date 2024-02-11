16e40f6818aa005da39e5951a7d535c837fa85c1
Dmytro Aleksandrov <alkersan@gmail.com>
2024-02-11 15:11:11 -0500

    p2 select, couldn't make the keyboard work

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import {getDisplayedValue} from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+const Select = ({label, value, onChange, children}) => {
+    const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (<Wrapper tabIndex={0}>
+            <NativeSelect value={value} onChange={onChange}>
+                {children}
+            </NativeSelect>
+            {displayedValue}
+            <IconWrapper>
+                <Icon id="chevron-down" strokeWidth={2} size={12} />
+            </IconWrapper>
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.div`
+    position: relative;
+    width: fit-content;
+    padding: 12px 16px;
+    border: none;
+    border-radius: 8px;
+    color: ${COLORS.gray700};
+    background-color: ${COLORS.transparentGray15};
+    
+    &:hover {
+        color: ${COLORS.black};
+    }
+`;
+
+const NativeSelect = styled.select`
+    opacity: 0;
+    position: absolute;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 100%;
+`;
+
+const IconWrapper = styled.div`
+    display: inline-block;
+    margin-left: 24px;
+    vertical-align: middle;
+`;
 
 export default Select;
